ceeb358fe0a716c69daa5d4f029cb996d5fefe08
yijun liu <yijun3@student.unimelb.edu.au>
2023-05-14 14:07:30 +1000

    add some api

--- back-end/mastodon_data.py
+++ back-end/mastodon_data.py
@@ -1,12 +1,12 @@
 from flask import Flask, jsonify
 import couchdb
-import json
 
 app = Flask(__name__)
 
 # connecting CouchDB
 couch = couchdb.Server('http://admin:1dTY1!PWM2@172.26.133.51:5984/')
 db = couch['mastodon_data_v2']
+db2 = couch['mastodon_data_v3']
 
 #Languages vs sentiment 
 @app.route('/mastondon_languages_sentiment')
@@ -55,5 +55,85 @@ def get_data():
         data1[i] = new_item
         
     return jsonify(data1)
+
+# RUwar_sentiment
+@app.route('/mastondon_RUwar_sentiment')
+def get_data5():
+    results = db.view('_design/sentiment/_view/RUwar', group=True)
+
+    data = {}
+    for row in results:
+        data[row.key] = row.value
+    return jsonify(data)
+
+# mag_sen_lang
+@app.route('/mastondon_mag_sen_lang')
+def get_data1():
+    results = db2.view('_design/war_sentiment/_view/magnitude_sentiment_lang', group=True)
+    data = {}
+    for row in results:
+        if row.key["language"] == None:
+            continue
+        new = {}
+        for i in row.value:
+            new[i] = row.value[i]
+
+        data[row.key["language"]] = new
+        
+    return jsonify(data)
+
+# post_hour_count
+@app.route('/mastondon_post_hour_count')
+def get_data2():
+    results = db2.view('_design/war_sentiment/_view/post_hour_count', group=True)
+    data = []
+    all_language = []
+    for row in results:
+        if row.key["language"] == None:
+            continue
+        if row.key["language"] not in all_language:
+            all_language.append(row.key["language"])
+        data.append(row)
+
+    data1 = {}
+    for i in all_language:
+        new = {}
+        for j in data:
+            if j.key["language"] == i:
+                new[j.key["hourRange"]] = j.value
+        data1[i] = new        
+    return jsonify(data1)
+
+# night_post_count
+@app.route('/mastondon_night_post_count')
+def get_data3():
+    results = db2.view('_design/war_sentiment/_view/night_post_count', group=True)
+    data = []
+    all_language = []
+    for row in results:
+        if row.key["language"] == None:
+            continue
+        if row.key["language"] not in all_language:
+            all_language.append(row.key["language"])
+        data.append(row)
+
+    data1 = {}
+    for i in all_language:
+        new = {}
+        for j in data:
+            if j.key["language"] == i:
+                new[j.key["hourRange"]] = j.value
+        data1[i] = new        
+    return jsonify(data1)
+
+# proportion_sentiment
+@app.route('/mastondon_proportion_sentiment')
+def get_data4():
+    results = db2.view('_design/war_sentiment/_view/proportion_sentiment', group=True)
+
+    data = {}
+    for row in results:
+        data[row.key] = row.value
+    return jsonify(data)
 if __name__ == '__main__':
     app.run(debug=True) 
--- back-end/twitter_data.py
+++ back-end/twitter_data.py
@@ -7,22 +7,25 @@ app = Flask(__name__)
 couch = couchdb.Server('http://admin:1dTY1!PWM2@172.26.133.51:5984/')
 db = couch['huge-twitter-v2']
 
-"""
-New South Wales
-Victoria
-Queensland
-South Australia
-Western Australia
-Tasmania
-Northern Territory
-Australian Capital Territory
-Other Territories
-"""
-
-# sentiment: state_sentiment
 with open('geoJSON files/states/STE_2021_AUST_GDA2020_Simplified.json', 'r') as file:
         state_geo = json.load(file)
-@app.route('/state_sentiment')
+with open('geoJSON files/gcc/GCCSA_2021_AUST_GDA2020.json', 'r') as file2:
+        gcc_geo = json.load(file2)
+with open('geoJSON files/postcode/POA_2021_AUST_GDA2020_Medium.json', 'r') as file3:
+        postcode_geo = json.load(file3)
+
+# sentiment: RUwar
+@app.route('/sentiment/RUwar')
+def get_data14():
+    results = db.view('_design/sentiment/_view/RUwar', group=True)
+
+    data = {}
+    for row in results:
+        data[row.key] = row.value
+    return jsonify(data)
+
+# sentiment: state_sentiment
+@app.route('/sentiment/state_sentiment')
 def get_points():
     results = db.view('_design/sentiment/_view/state_sentiment', group=True)
 
@@ -82,11 +85,8 @@ def get_points():
 
     return jsonify(state_sentiment_output)
 
-# sentiment: gcc_sentiment
-with open('geoJSON files/gcc/GCCSA_2021_AUST_GDA2020.json', 'r') as file2:
-        gcc_geo = json.load(file2)
-        
-@app.route('/gcc_sentiment')
+# sentiment: gcc_sentiment        
+@app.route('/sentiment/gcc_sentiment')
 def get_points1():
     results = db.view('_design/sentiment/_view/sum_sent_gcc', group=True)
     results1 = db.view('_design/sentiment/_view/ave_sent_gcc', group=True)
@@ -120,10 +120,7 @@ def get_points1():
     return jsonify(gcc_sentiment_output)
 
 # sentiment: postcode_sentiment
-with open('geoJSON files/postcode/POA_2021_AUST_GDA2020_Large.json', 'r') as file3:
-        postcode_geo = json.load(file3)
-
-@app.route('/postcode_sentiment')
+@app.route('/sentiment/postcode_sentiment')
 def get_points2():
     # Queries the view and returns data
     results = db.view('_design/sentiment/_view/war_postcode', group=True)
@@ -181,7 +178,7 @@ def get_points2():
     
 
 # sentiment: Languages vs sentiment
-@app.route('/twitter_languages_sentiment')
+@app.route('/sentiment/twitter_languages_sentiment')
 def get_data1():
     # Queries the view and returns data
     results = db.view('_design/sentiment/_view/war_sent_lang', group=True)
@@ -226,8 +223,46 @@ def get_data1():
         
     return jsonify(data1)
 
+# sentiment: war_monthly_state_proportion
+@app.route('/sentiment/war_monthly_state_proportion')
+def get_data15():
+    results = db.view('_design/sentiment/_view/war_monthly_state_proportion', group=True)
+
+    data = []
+    all_month = []
+    for row in results:
+        if row.key[0] == None:
+            continue
+        if row.key[0] not in all_month:
+            all_month.append(row.key[0])
+        data.append(row)
+
+    data1 = {}
+    for i in all_month:
+        dic = {}
+        all_state = []
+        for jj in data:
+            if jj.key[0] ==i:
+                if jj.key[1] == None:
+                    continue
+                if jj.key[1] not in all_state:
+                    all_state.append(jj.key[1])
+        
+        for g in all_state:
+            new = {"Negative":0,"Neutral":0, "Positive":0}
+            for j in data:
+                if j.key[1] == None:
+                    continue
+                if j.key[0] == i and j.key[1] == g:
+                    new[j.key[2]] += j.value
+            dic[g] = new
+
+        data1[i] = dic
+
+    return jsonify(data1)
+
 # sentiment: magnitude of sentiment by state
-@app.route('/mag_sentiment_state')
+@app.route('/sentiment/mag_sentiment_state')
 def get_data3():
     results = db.view('_design/sentiment/_view/mag_sent_lan_state', group=True)
 
@@ -271,7 +306,7 @@ def get_data3():
     return jsonify(output)
 
 # sentiment: magnitude of sentiment by postode
-@app.route('/mag_sentiment_postcode')
+@app.route('/sentiment/mag_sentiment_postcode')
 def get_data4():
     results = db.view('_design/sentiment/_view/mag_sent_lan_postcode', group=True)
 
@@ -315,7 +350,7 @@ def get_data4():
     return jsonify(output)
 
 # general: tweet_lang_hourRange
-@app.route('/tweet_lang_hourRange')
+@app.route('/general/tweet_lang_hourRange')
 def get_data10():
     results = db.view('_design/general/_view/tweet_postcode', group=True)
 
@@ -342,7 +377,7 @@ def get_data10():
     return jsonify(data1)
 
 # general: tweet_lang_state
-@app.route('/tweet_lang_state')
+@app.route('/general/tweet_lang_state')
 def get_data5():
     results = db.view('_design/general/_view/tweet_lang_state', group=True)
 
@@ -389,10 +424,58 @@ def get_data5():
 
     return jsonify(output)
 
+# general: tweet_lang_postcode
+@app.route('/general/tweet_lang_postcode')
+def get_data16():
+    results = db.view('_design/general/_view/tweet_lang_postcode', group=True)
 
+    data = []
+    all_postcode = []
+    output = {"type": "FeatureCollection","features": []}
+    for row in results:
+        if row.key["postcode"] == None:
+            continue
+        if row.key["postcode"] not in all_postcode:
+            all_postcode.append(row.key["postcode"])
+        data.append(row)
+
+    data1 = {}
+    for i in all_postcode:
+        dic = {}
+        all_lang = []
+        for jj in data:
+            if jj.key["postcode"] ==i:
+                if jj.key["language"] not in all_lang:
+                    all_lang.append(jj.key["language"])
+        
+        for g in all_lang:
+            new = {}
+            for j in data:
+                if j.key["postcode"] == i and j.key["language"] == g:
+                    new[j.key["hourRange"]] = j.value
+            dic[g] = new
+
+        data1[i] = dic
+    
+    with open('json_output_tweet_lang_postcode.json', 'w') as f:
+        json.dump(data1, f)
+
+    for feature in postcode_geo['features']:
+        for row in data1:
+            if feature["properties"]['POA_CODE21'].lower() == row:
+                for i in data1[row]:
+                    feature["properties"][i] = data1[row][i]
+                output["features"].append(feature)
+
+    with open('geo_output_tweet_lang_postcode.json', 'w') as file:
+        json.dump(output, file)
+
+    
+
+    return jsonify(output)
 
 # lgbt: lgbt_sent_postcode
-@app.route('/lgbt_sent_postcode')
+@app.route('/lgbt/lgbt_sent_postcode')
 def get_points6():
     # Queries the view and returns data
     results = db.view('_design/lgbt/_view/lgbt_sent_postcode', group=True)
@@ -446,7 +529,7 @@ def get_points6():
     return jsonify(output)
 
 # lgbt: mag_sent_lan_postcode
-@app.route('/mag_sentiment_lan_postcode')
+@app.route('/lgbt/mag_sentiment_lan_postcode')
 def get_data7():
     results = db.view('_design/lgbt/_view/mag_sent_lan_postcode', group=True)
 
@@ -488,6 +571,80 @@ def get_data7():
         json.dump(output, file33)
         
     return jsonify(output)
+# lgbt: monthly_state
+@app.route('/lgbt/monthly_state')
+def get_data12():
+    results = db.view('_design/lgbt/_view/lgbt_monthly_state', group=True)
+
+    data = []
+    all_month = []
+    for row in results:
+        if row.key[0] == None:
+            continue
+        if row.key[0] not in all_month:
+            all_month.append(row.key[0])
+        data.append(row)
+
+    data1 = {}
+    for i in all_month:
+        dic = {}
+        all_state = []
+        for jj in data:
+            if jj.key[0] ==i:
+                if jj.key[1] == None:
+                    continue
+                if jj.key[1] not in all_state:
+                    all_state.append(jj.key[1])
+        
+        for g in all_state:
+            new = {"Negative":0,"Neutral":0, "Positive":0}
+            for j in data:
+                if j.key[1] == None:
+                    continue
+                if j.key[0] == i and j.key[1] == g:
+                    new[j.key[2]] += j.value
+            dic[g] = new
+
+        data1[i] = dic
+
+    return jsonify(data1)
+
+# lgbt: monthly_state_mag
+@app.route('/lgbt/monthly_state_mag')
+def get_data13():
+    results = db.view('_design/lgbt/_view/lgbt_monthly_state_mag', group=True)
+
+    data = []
+    all_month = []
+    for row in results:
+        if row.key["month"] not in all_month:
+            all_month.append(row.key["month"])
+        data.append(row)
+
+    data1 = {}
+    for i in all_month:
+        dic = {}
+        all_state = []
+        for jj in data:
+            if jj.key["month"] ==i:
+                if jj.key["state"] == None:
+                    continue
+                if jj.key["state"] not in all_state:
+                    all_state.append(jj.key["state"])
+        
+        for g in all_state:
+            new = {}
+            for j in data:
+                if j.key["state"] == None:
+                    continue
+                if j.key["month"] == i and j.key["state"] == g:
+                    for t in j.value:
+                        new[t] = j.value[t]
+            dic[g] = new
+
+        data1[i] = dic
+    return jsonify(data1)
+
 
 if __name__ == '__main__':
     app.run(debug=True) 
