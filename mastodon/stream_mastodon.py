c3fdd269466d8d730b001ee7e7e256e246997e84
aoboli1231 <1744009357@qq.com>
2023-05-21 22:09:56 +1000

    add contributer

--- mastodon/stream_mastodon.py
+++ mastodon/stream_mastodon.py
@@ -1,3 +1,12 @@
+# COMP90024 Project 2 2023
+# Contributer
+# Aobo Li              1172339
+# Pavith Samarakoon    1297058
+# Zhihao Liang         1367102
+# Jiqiang Chen         1171420
+# Yijun Liu            1132416
+
+
 from mastodon import Mastodon, MastodonNotFoundError, MastodonRatelimitError, StreamListener, MastodonNetworkError
 import csv, os, time, json
 import re
@@ -131,7 +140,7 @@ class Listener(StreamListener):
         }
 
         # Write the processed data to couchdb
-        db.save(processed_data)
+        #db.save(processed_data)
 
 while True:
     try:
